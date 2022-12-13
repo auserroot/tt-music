47025a26bc81b72322d68aa379c2e020a7bca811
unknown <fauserfzy@gmail.com>
2022-12-13 16:09:18 +0800

    build

--- env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-interface ImportMetaEnv {
-    readonly VITE_APP_TITLE:string,
-    readonly VITE_API_BASE :string,//baseurl
-}
-
-interface ImportMate {
-    readonly env:ImportMetaEnv
-}
